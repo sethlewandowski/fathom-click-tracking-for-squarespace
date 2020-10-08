4a5aed82a3e064506a335b0eba9ab3a5caeb25f5
Seth Lewandowski <lewandowski.seth@gmail.com>
2020-10-08 11:39:19 -0700

    tracking.js

---
+++ tracking.js
@@ -0,0 +1,23 @@
+<script>
+
+// Click tracking for any element going to prevail.com
+document.querySelectorAll("[href*=prevail]").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('RRWE7BFH', 0));
+});
+
+// Click tracking for any element going to qualis.com
+document.querySelectorAll("[href*=qualis]").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('ZKIMOOG1', 0));
+});
+
+// Click tracking for any element going to pdcrx.com
+document.querySelectorAll("[href*='pdcrx']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('INZHVC6X', 0));
+});
+
+// Click tracking for any element going to twinmed.com
+document.querySelectorAll("[href*='twinmed']").forEach(item => {
+	item.addEventListener('click', () => fathom.trackGoal('IIA3MROJ', 0));
+});
+
+</script>
